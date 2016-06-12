04fdefc7eab0820e7dc2740374f74cf9ead33446
Jonny Pickard <jonathandpickard@gmail.com>
2016-06-12 18:50:55 +0100

    Building graph

--- minglish.py
+++ minglish.py
@@ -1,16 +1,43 @@
 def answer(words):
-    return alphabet_size(words)
+    graph = build_graph(words)
 
-#build graph
-def alphabet_size(words):
+    build_edges(words, graph)
+
+
+#size of alphabet
+def build_graph(words):
     characters = []
-    alphabet = []
+    alphabet = {}
 
     for word in words:
         characters += list(word)
 
     for char in characters:
         if not any(char in a for a in alphabet):
-            alphabet += char
+            alphabet[char] = []
 
-    return len(alphabet)
+    return alphabet
+
+#compare letters between words to build edges
+def build_edges(words, graph):
+    graph = graph
+    count = 0
+    length = len(words)
+
+    for i in range(len(words)):
+        n = 0
+        print i
+        print (words[count])[n]
+
+        while i != length -1:
+            if (words[count])[n] == (words[count + 1])[n]:
+                n += 1
+                count += 1
+                print "here"
+            else:
+                graph[(words[count])[n]] += (words[count + 1])[n]
+                n = 0
+                count += 1
+                print "here now"
+
+    print graph
